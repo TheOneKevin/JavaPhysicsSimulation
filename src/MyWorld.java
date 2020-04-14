0209a127b41678eaf9f2de01b210ae88fed80ef8
Mustafa Mesrefoglu <mamesrefoglu@gmail.com>
2020-04-14 16:33:32 -0400

    I did a whoopsie

--- src/MyWorld.java
+++ src/MyWorld.java
@@ -8,7 +8,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -16,6 +15,6 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1);
     }
 }
