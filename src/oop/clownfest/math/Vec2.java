87d87f72c206300389e50a33c84858e711ff890b
NovaTerra <hkq349577346@gmail.com>
2020-04-14 20:01:12 -0400

    Added some basic vector and matrix classes

---
+++ src/oop/clownfest/math/Vec2.java
@@ -0,0 +1,51 @@
+package oop.clownfest.math;
+
+/**
+ * 2-tuples in R^2 and their operations.
+ *
+ * @author Kai Qi Hao (nova-terra)
+ * @version April 14th, 2020
+ */
+public class Vec2
+{
+    private double x, y;
+
+    public Vec2(double x, double y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+
+    public double getX()
+    {
+        return x;
+    }
+
+    public double getY()
+    {
+        return y;
+    }
+
+    public void add(Vec2 u)
+    {
+        this.x+=u.x;
+        this.y=u.y;
+    }
+
+    public void invert()
+    {
+        this.x=-this.x;
+        this.y=-this.y;
+    }
+
+    public static double getEucInnerProduct(Vec2 u, Vec2 v)
+    {
+        return u.x*v.x+u.y*v.y;
+    }
+
+    public void LT(Mat2 A)
+    {
+        this.x=this.x*A.getValueAt(0,0)+this.y*A.getValueAt(0,1);
+        this.y=this.x*A.getValueAt(1,0)+this.y*A.getValueAt(1,1);
+    }
+}
