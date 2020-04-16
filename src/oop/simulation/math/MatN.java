eb1cced5d43d2b14a36ba6359a49941e26a5b375
NovaTerra <hkq349577346@gmail.com>
2020-04-15 22:50:47 -0400

    Adjusted the functionality of matrix and vector classes; added more functions.

---
+++ src/oop/simulation/math/MatN.java
@@ -0,0 +1,77 @@
+package oop.simulation.math;
+
+/**
+ * Matrix representations of T.
+ * T: R^n -> R^m.
+ * <p>
+ * (Yes I'm well aware that using VecN to implement MatN would be a better idea but I'm too lazy to change my code)
+ * <p>
+ * also I will add documentations later this week
+ *
+ * @author Kai Qi Hao (nova-terra)
+ */
+public class MatN {
+    private double[][] data;
+
+    public MatN(int m, int n) {
+        if (m > 0 && n > 0) {
+            data = new double[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    data[i][j] = 0;
+                }
+            }
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public MatN(double[][] input) {
+        if (input != null) {
+            data = new double[input.length][input[0].length];
+            for (int i = 0; i < input.length; i++) {
+                for (int j = 0; j < input[0].length; j++) {
+                    data[i][j] = input[i][j];
+                }
+            }
+        } else {
+            throw new NullPointerException();
+        }
+    }
+
+    public MatN(MatN other) {
+        data = other.data;
+    }
+
+    public VecN getRowData(int row) {
+        if (row > 0 && row < data.length + 1) {
+            return new VecN(data[row - 1]);
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public VecN getColumnData(int column) {
+        if (column > 0 && column < data[0].length + 1) {
+            double[] v = new double[data.length];
+            for (int i = 0; i < data.length; i++) {
+                v[i] = data[i][column - 1];
+            }
+            return new VecN(v);
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public int getNumberOfRows() {
+        return data.length;
+    }
+
+    public int getNumberOfColumns() {
+        return data[0].length;
+    }
+
+    public double[][] getArrayData() {
+        return data;
+    }
+}
---
+++ src/oop/simulation/math/VecN.java
@@ -0,0 +1,126 @@
+package oop.simulation.math;
+
+/**
+ * n-tuples in R^n and their operations.
+ * <p>
+ * <p>
+ * will add documentations this week
+ *
+ * @author Kai Qi Hao (nova-terra)
+ */
+public class VecN {
+    private double[] data;
+
+    public VecN(int dimension) {
+        if (dimension > 0) {
+            data = new double[dimension];
+            for (double i : data) {
+                i = 0;
+            }
+        } else {
+            throw new IndexOutOfBoundsException("Dimensions must be greater than 0.");
+        }
+    }
+
+    public VecN(double[] inputArray) {
+        if (inputArray != null) {
+            data = new double[inputArray.length];
+            data = inputArray;
+        } else {
+            throw new NullPointerException();
+        }
+    }
+
+    public VecN(VecN other) {
+        data = other.data;
+    }
+
+    public static VecN add(VecN u, VecN v) {
+        if (u.data.length != v.data.length) {
+            throw new IndexOutOfBoundsException("Vector dimensions are not equal.");
+        } else {
+            double[] w = new double[u.data.length];
+            for (int i = 0; i < u.data.length; i++) {
+                w[i] = u.data[i] + v.data[i];
+            }
+            return new VecN(w);
+        }
+    }
+
+    public static double getEuclideanInnerProduct(VecN u, VecN v) {
+        if (u.data.length != v.data.length) {
+            throw new IndexOutOfBoundsException("Vector dimensions are not equal.");
+        } else {
+            double result = 0;
+            for (int i = 0; i < u.data.length; i++) {
+                result += u.data[i] * v.data[i];
+            }
+            return result;
+        }
+    }
+
+    public static double getEntryAt(VecN u, int position) {
+        return u.getEntryAt(position);
+    }
+
+    public void add(VecN u) {
+        if (u.data.length != data.length) {
+            throw new IndexOutOfBoundsException("Vector dimensions are not equal.");
+        } else {
+            for (int i = 0; i < data.length; i++) {
+                data[i] += u.data[i];
+            }
+        }
+    }
+
+    public void invert() {
+        for (int i = 0; i < data.length; i++) {
+            data[i] = 0 - data[i];
+        }
+    }
+
+    public int getDimension() {
+        return data.length;
+    }
+
+    public double getEntryAt(int position) {
+        if (position > 0 && position < data.length + 1) {
+            return data[position - 1];
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public double[] getArrayData() {
+        return data;
+    }
+
+    public void matrixTransform(MatN T) {
+        if (data.length == T.getNumberOfColumns()) {
+            double[] v = new double[T.getNumberOfRows()];
+            for (int i = 0; i < T.getNumberOfRows(); i++) {
+                v[i] = getEuclideanInnerProduct(T.getRowData(i + 1), this);
+            }
+            copyArray(v);
+        } else {
+            throw new ArithmeticException();
+        }
+    }
+
+    public String toString() {
+        String output = "";
+        output += "(";
+        for (int i = 0; i < data.length; i++) {
+            output += Double.toString(data[i]);
+            if (i != data.length - 1) {
+                output += ", ";
+            }
+        }
+        output += ")";
+        return output;
+    }
+
+    private void copyArray(double[] arr) {
+        data = arr;
+    }
+}
